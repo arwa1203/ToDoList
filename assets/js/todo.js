8c28e3c207eafa15d34ab04d45ada0915a62300e
Arwa <arwa.ahmed1203@gmail.com>
2018-06-02 06:43:41 -0500

    Created functional to-do-list with very minimal styling

---
+++ assets/js/todo.js
@@ -0,0 +1,52 @@
+/* Variables */
+
+/* Code to execute */
+
+addDelete();
+checkOff();
+inputEnter();
+
+/*Functions*/
+
+function addDelete(){
+	$("ul").on("click", ".delete",function(e){
+
+		// fade and delete element
+		
+		$(this).parent().fadeOut(500, function(){
+			$(this).remove();
+		});
+		e.stopPropagation();
+	});
+
+}
+
+function checkOff(){
+	$("ul").on("click", "li", function(){
+		$(this).toggleClass("completed");
+	});
+}
+
+function inputEnter(){
+
+	// capture what the user types into the input field after hitting enter
+	$("input[type='text']").on("keypress", function(event){
+
+		if(event.which === 13){
+			
+			$("ul").append(makeLiLine($(this).val()));
+			$(this).val("");
+		}
+
+
+	});
+
+}
+
+function makeLiLine(task){
+	console.log(task);
+	var str = "<li><span class=\"delete\">X</span>" + task + "</li>";
+	return str;
+
+
+}
